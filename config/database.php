855a36941e0f37ba909d6769fd1f50430610a94d
Jorge Silva <jnashvs@gmail.com>
2019-10-07 00:58:31 +0100

    6 commit

--- config/database.php
+++ config/database.php
@@ -39,14 +39,13 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'epiz_24595786_blog'),
+            'username' => env('DB_USERNAME', 'epiz_24595786'),
+            'password' => env('DB_PASSWORD', 'carolainynashvs1996'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
